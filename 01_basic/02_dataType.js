358fd8abc216337e091b9a3a09fd7090db7f24d9
asleep002 <jaryalG.Manish@outlook.com>
2024-08-06 15:40:20 +0000

    dataType

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,29 @@
+"use strict";//this will tell the compiler to treat all the code as the new version 
+
+//alert(3+3) we r using node js not the browser 
+
+console.log(3+3)
+
+let name = "manish jaryal"
+let age=22
+let isLogedIn=false 
+
+console.table([name,age,isLogedIn])
+
+// data type in js 
+//primitive datatype 
+
+//number 
+//bigint
+//string -> ""
+//boolean
+//null-> standalone value
+//undefined -> value not assigned yet 
+// symbol ->for uniquness 
+
+// object 
+
+console.log(typeof null)
+console.log(typeof undefined)
+
+
