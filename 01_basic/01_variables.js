835c97fef5ea52ac1e6f5a2bd02d211db184fcac
asleep002 <jaryalG.Manish@outlook.com>
2024-08-06 14:56:14 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144453
+let accountEmail = "manishjaryal51@gmail.com"
+var accountPassword= "12345"
+accountCity = "pune"// possible to create variable 
+let accountState;// if the variable is not intilised them it's value will be undefined 
+
+
+
+//accountId=2 not allowed  const can't be re-assigned 
+
+accountEmail="hcch@gmail.com"
+accountPassword="6969"
+accountCity="delhi"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var 
+becuse of the issue in the block scope and functional scope 
+
+
+*/
