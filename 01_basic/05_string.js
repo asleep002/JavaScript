139abf7d83fba7ac562251a46c748f86959c929c
asleep002 <jaryalG.Manish@outlook.com>
2024-08-11 18:16:58 +0000

    String

---
+++ 01_basic/05_string.js
@@ -0,0 +1,53 @@
+const name="Manish"
+const repoCount=50
+// to concadente the stings don't use these kinda thing ...these r old nd outdated
+// console.log(name+repoCount+"  hello World!") 
+
+// console.log(`hello my name is ${name} and my repo count is ${repoCount} `);
+// use back ticks insted ... in those backticks is u want to add varible the use ${}
+//this will also produce same output but the more readable
+// it is known as string interpolation
+
+// way to declare string use new keyword a object is crearted 
+// string contain many methods 
+
+ const gameName=new String("Manish-MJ-")
+
+// console.log(gameName[0]);
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase);
+
+// console.log(gameName.charAt(2));
+
+// console.log(gameName.indexOf('h'))
+
+// const newstring = gameName.substring(0,4)
+
+// const anotherString=gameName.slice(-8,4)
+
+const newStingone="     Manish    "
+console.log(newStingone)
+console.log(newStingone.trim())
+
+const url="https://manish.com/manish%20jaryal"//changing in url
+
+
+url.replace('%20',"-")
+
+
+console.log(gameName.split('-'))
+
+
+
+
+
+
+
+
+
+
+
