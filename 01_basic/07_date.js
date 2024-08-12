6d9fd7ea7a5c32d02e48eb76f5282b94c359c3de
asleep002 <jaryalG.Manish@outlook.com>
2024-08-12 17:12:07 +0000

    date  & TIME

---
+++ 01_basic/07_date.js
@@ -0,0 +1,15 @@
+
+let myDate=new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString())
+// console.log(typeof myDate)//date is the object type in js 
+
+//let myCreatedDate=new Date(2024,5,25)//month start form 0 in js 
+// let myCreatedDate=new Date(2024,5,25,5,3)
+let myCreatedDate=new Date("2024-01-14")
+// console.log(myCreatedDate.toDateString())
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
