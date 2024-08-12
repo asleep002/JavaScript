a97eba705dc21b0912a246faa4ef7bf22a6ef8d0
asleep002 <jaryalG.Manish@outlook.com>
2024-08-12 16:28:57 +0000

    number & Math lib

---
+++ 01_basic/06_number.js
@@ -0,0 +1,20 @@
+const score=100
+
+const balance=new Number(500)
+ 
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(3))// gives the percision value after the number 
+
+const number=233.36589
+// console.log(number.toFixed(2));
+// console.log(number.toPrecision(3));//number of digits to show  with round of to the next digits avaliable
+
+const hunderds=1000000
+// console.log(hunderds.toLocaleString('en-IN'))
+//+++++++++++++++++++++++++ maths +++++++++++++++++
+
+// console.log(Math.abs(-9))
+console.log((Math.random())*10)//it give the value bet. 0 to 1 
