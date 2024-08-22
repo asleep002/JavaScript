0fd61ba7d59890797eb612167e997bba0011c119
Manish Jaryal <jaryalG.Manish@outlook.com>
2024-08-22 17:01:51 +0000

    FUNCTIONS

---
+++ 03_basic/01_function.js
@@ -0,0 +1,30 @@
+// //func declaration and defination 
+
+// function sayMyName(){
+//     console.log("M")
+//     console.log("A")
+//     console.log("N")
+//     console.log("I")
+//     console.log("S")
+//     console.log("H")
+// }
+
+// sayMyName()//call...refrence...execution
+
+
+
+//add two numbers
+//need not to give type
+
+function addTwoNumber(number1,number2)
+{
+// console.log(number1+number2)
+// return number1+number2
+let result=number1+number2
+return result
+
+}
+
+let result=addTwoNumber(5,6)
+
+console.log(result)
