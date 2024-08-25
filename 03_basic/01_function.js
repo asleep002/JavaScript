b14800342a35d18e55755caab700d3f0074bd407
Manish Jaryal <jaryalG.Manish@outlook.com>
2024-08-25 11:58:02 +0000

    func object and array

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -32,7 +32,7 @@ const result=addTwoNumber(5,6)
 // console.log(result)
 
 function loginUserMessage(username ="sam"){//default value is passed here as sam means if no input is given then it use samm 
-    
+
     if(username== undefined){
         console.log("plz enter your name")
     }
@@ -44,3 +44,43 @@ function loginUserMessage(username ="sam"){//default value is passed here as sam
 const message=loginUserMessage()//if nothing is passed as the argument
 
 console.log(message)
+
+//rest and spread operator
+
+// in this rest parameter is passed to the function ...in case u r getting then multiple values nd u don't know much more r to come then we use this ...it will return the value in the array format 
+
+function calculateCartPrice(...num1){
+    return num1;
+
+}
+
+const user={
+    username:"some_user",
+    price:6000
+}
+
+function handleObject(anyobject){
+    // console.log(`username is ${anyobject.username} and the price is ${anyobject.price}`)
+return  `username is ${anyobject.username} and the price is ${anyobject.price}`
+
+}
+
+console.log(handleObject(user));
+
+console.log(handleObject({
+    username:"another_user",
+    price:1900
+    
+}));
+
+//array passed in the array 
+
+const newArray=[222,300,500,600]
+function returnSecondValue(getArray){
+    return getArray[1];
+
+}
+
+console.log(returnSecondValue(newArray));
+
+
