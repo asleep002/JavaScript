55ca3990e6fd0377dc3f3d3ebbe46a837f079d41
Manish Jaryal <jaryalG.Manish@outlook.com>
2024-08-27 11:36:21 +0000

    function

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -11,3 +11,33 @@ if(true){
 //global scope in node nd in window
 
 
+function one(){
+    const username="Manish Jarayal"
+
+    function two(){
+        const website="Youtube"
+        console.log(username)
+    }
+    // console.log(website)//cant' be accessed
+    two()
+}
+
+one()
+// clouser....?
+
+
+// +++++++++++++++intersting+++++++++++++++++++
+console.log(addone(5));
+
+function addone(num){
+    return num+1;
+
+}
+
+
+// console.log(addone(6))
+// console.log(addTWo(2));//this type of function can't be declared 
+const addTWo=function(num){
+    return num+2
+}
+
