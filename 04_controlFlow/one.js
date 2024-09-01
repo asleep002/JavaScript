d5ff1f28668d4248090c559f5d450d0b3cbae0a3
Manish Jaryal <jaryalG.Manish@outlook.com>
2024-09-01 08:40:48 +0000

    controlflow

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,72 @@
+// // if
+// const  isLoggedIn=true
+
+// if(isLoggedIn){
+//     console.log("hello world!");
+    
+// }
+
+// //== it check value equal or not 
+// // === ..... !== also check the type of value 
+
+// if(2=='2'){
+//     console.log("executed")
+
+// }
+
+// if(2=='9'){
+//     console.log("done")
+// }
+
+// if(2==='2'){
+//     console.log("type checked...")
+// }
+
+
+// if(2!=='2'){
+//     console.log("type checked...")
+// }
+// //<,>,<=,>=,==,!=,===,!==
+
+// // if else block 
+
+// const temp=21
+// if(temp>30){
+//     console.log("high temp");
+    
+// }
+// else{
+//     console.log("moderate temp  ");
+    
+// }
+
+// const score=200
+
+// if(score>100){
+//     const power="fly"
+//     console.log(`the power is ${power}`);
+    
+// }
+
+// console.log(`the power is ${power}`);
+
+// const balance=1000
+
+// if(balance<500) console.log("balance is less than 1000");
+
+// if(balance<500);
+//  console.log("balance is less than 1000");
+
+
+//single line mei execute ho rha hai semi-colon zaroori hai 
+//way to write above with multiple lines 
+
+// if(balance>500) console.log("balance is more than 1000"),
+// console.log("next line ");
+
+    
+
+
+
+
+
