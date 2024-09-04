c3f75d0eb62a147efb8b020ea63867df92b2a89c
Manish Jaryal <jaryalG.Manish@outlook.com>
2024-09-04 05:22:05 +0000

    loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,41 @@
+//for loop 
+
+
+// for (let i = 0; i <10; i++) {
+//     const element = i;
+
+//     if(element==5){
+//         console.log("5 is best number ");
+        
+//     }
+//     console.log(element);
+  
+// }
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(`outer loop : ${i}`)
+//      for (let j = 0; j < 5; j++) {
+//         console.log(`inner loop : ${j}`)
+//      }
+    
+// }
+
+// let  myArray=["flash","batman","suprman",5,6,9]
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+    
+// }
+
+// break and continue 
+for (let index = 0; index < 10; index++) {
+
+    if(index==5){
+        console.log(` 5 detected `);
+        break;
+        
+    }
+    console.log(` ${index}`)
+    
+}
