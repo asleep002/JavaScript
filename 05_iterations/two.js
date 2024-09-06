b840a3846e8b44e0ca8c6d828f30ba0703fcd13f
Manish Jaryal <jaryalG.Manish@outlook.com>
2024-09-06 00:33:29 +0000

    loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+// let index=0
+// while(index<=10){
+//     console.log(`value of the index is ${index}`)
+//     index=index+1;
+// }
+
+// let myArray=['iron-man','capt. America','thor']
+// let index=0
+// while(index<myArray.length){
+//     console.log(`value is ${myArray[index]}`);
+//     index++
+    
+// }
+
+let score=11;
+do{ 
+    console.log(`score is ${score}`);
+    
+}while(score<10);
